0a081dfffabb83f89f05537c44db72076708951e
Merl J Creps Jr <merl.crepsjr@bradyplc.com>
2018-02-05 19:49:51 -0500

    Added Object Example Insters in a Loop

--- EET4250Spring2018/src/InsertCourses.java
+++ EET4250Spring2018/src/InsertCourses.java
@@ -9,35 +9,34 @@ import org.slf4j.LoggerFactory;
 public class InsertCourses {
 
 	private static Logger logger = LoggerFactory.getLogger(runnable.class);
-	
+
 	public void inserts(Connection connection) {
-		
+
 		Scanner scanner = new Scanner(System.in);
-		
+
 		logger.debug("Entering inserts.");
 
-		String sql = "INSERT INTO Course ( " + 
-		" CourseId, InstructorId, CreditHrs, Semester, Year ) " + 
-		" VALUES (?, ? ,?, ?, ?) ";
-		
-		int rowsAffected=0;
-		
-		for (int x=0;x<9;x++) {
+		String sql = "INSERT INTO Course ( " + " CourseId, InstructorId, CreditHrs, Semester, Year ) "
+				+ " VALUES (?, ? ,?, ?, ?) ";
+
+		int rowsAffected = 0;
+
+		for (int x = 0; x < 9; x++) {
 			System.out.print("Enter Course Id: ");
 			String coursId = scanner.nextLine();
-			
+
 			System.out.print("Enter Instructor Id: ");
 			int instructorId = Integer.parseInt(scanner.nextLine());
-			
+
 			System.out.print("Enter Credit Hours: ");
-			int creditHours = Integer.parseInt(scanner.nextLine());			
-			
+			int creditHours = Integer.parseInt(scanner.nextLine());
+
 			System.out.print("Enter Semester: ");
 			String semester = scanner.nextLine();
-			
+
 			System.out.print("Enter Year Offered: ");
 			int yearOffered = Integer.parseInt(scanner.nextLine());
-			
+
 			try {
 				PreparedStatement ps = connection.prepareStatement(sql);
 				ps.setString(1, coursId);
@@ -45,12 +44,32 @@ public class InsertCourses {
 				ps.setInt(3, creditHours);
 				ps.setString(4, semester);
 				ps.setInt(5, yearOffered);
-				rowsAffected =+ ps.executeUpdate();
-			}
-			catch (SQLException e) {
+				rowsAffected = +ps.executeUpdate();
+			} catch (SQLException e) {
 				logger.error(e.toString());
 			}
-		}	
-		logger.debug("Rows Affected: "+rowsAffected);
+		}
+		logger.debug("Rows Affected: " + rowsAffected);
+	}
+
+	public void inserts(Connection connection, Course course) {
+
+		logger.debug("Entering inserts.");
+		String sql = "INSERT INTO Course ( " + " CourseId, InstructorId, CreditHrs, Semester, Year ) "
+				+ " VALUES (?, ? ,?, ?, ?) ";
+
+		int rowsAffected = 0;
+		try {
+			PreparedStatement ps = connection.prepareStatement(sql);
+			ps.setString(1, course.getCourseId());
+			ps.setInt(2, course.getInstructorId());
+			ps.setInt(3, course.getCreditHrs());
+			ps.setString(4, course.getSemester());
+			ps.setInt(5, course.getYear());
+			rowsAffected = +ps.executeUpdate();
+		} catch (SQLException e) {
+			logger.error(e.toString());
+		}
+		logger.debug("Rows Affected: " + rowsAffected);
 	}
 }
